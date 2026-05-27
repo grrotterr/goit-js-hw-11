29d04db370ac323fb41ebd93f20c64d6ffed8fd2
grrotterr <grrotterr@mail.com>
2026-05-27 23:36:11 +0200

    hw_11_2

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import getImagesByQuery from './pixabay-api.js';
+import getImagesByQuery from './js/pixabay-api.js';
 
 import {
   createGallery,
   clearGallery,
   showLoader,
   hideLoader,
-} from './render-functions.js';
+} from './js/render-functions.js';
 
 const form = document.querySelector('.form');
 const input = form.elements['search-text'];
 
 form.addEventListener('submit', handleSubmit);
 
-// запуск по URL при загрузке страницы
+
 document.addEventListener('DOMContentLoaded', () => {
   const params = new URLSearchParams(window.location.search);
   const urlQuery = params.get('search-text');
